e16c3a41b4e386431b025c814e390976e2d844a5
qamar345 <qamargill427@gmail.com>
2023-12-21 15:22:13 +0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { GridGallery } from "./Components/GridGallery";
+import { MoveBg } from "./Components/MoveBg";
+import { SlideShow } from "./Components/SlideShow";
+import { SplideSlide } from "./Components/SplideSlide";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <SlideShow />
+      <GridGallery/> */}
+      {/* <SplideSlide/> */}
+      <MoveBg/>
+    </>
   );
 }
 
---
+++ src/Components/MoveBg.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Style/Bg.css"
+
+export const MoveBg = () => {
+
+    
+
+  return (
+    <>
+    
+    <div id='bg'>
+        <div id='inner-bg'></div>
+    </div>
+    </>
+  )
+}
